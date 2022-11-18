492612e61309041d549df6adb22486a28eb39f95
Alek Ivankovic <alek.ivankovic@gmail.com>
2022-11-18 17:08:16 +0800

    Add files via upload

---
+++ Fredproj.WebApi/Models/Person.cs
@@ -0,0 +1,30 @@
+﻿namespace Fredproj.WebApi.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public bool Exists { get; set; }
+    }
+
+    public interface IPersonRegister
+    {
+        void AddPerson(Person person);
+
+        void UpdatePerson(Person person);
+    }
+
+    public class SillyPersonRegister : IPersonRegister
+    {
+        public void AddPerson(Person person)
+        {
+            person.Exists = true;
+        }
+
+        public void UpdatePerson(Person person)
+        {
+            person.Exists = false;
+        }
+    }
+
+}
